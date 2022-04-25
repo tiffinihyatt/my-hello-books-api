ee50db7a28c730f669f6fd6a1d083ca3b7a92bba
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-24 19:42:51 -0500

    Created helper function to validate book id

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -12,6 +12,19 @@ books = [
     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
 ]
+
+def validate_book(book_id):
+    # try/except to confirm that book_id is numeric
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    
+    for book in books:
+        if book.id == book_id:
+            return book
+    
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
@@ -29,19 +42,11 @@ def handle_books():
     return jsonify(books_response)
 
 @books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    # try/except to confirm that book_id is numeric
-    try:
-        book_id = int(book_id)
-    except:
-        return {"message": f"book {book_id} not found"}, 404
-
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+def handle_single_book(book_id):
+    book = validate_book(book_id)
     
-    return {"message": f"book {book_id} not found"}, 404
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
