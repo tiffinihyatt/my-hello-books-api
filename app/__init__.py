abbed33d3db0241c70b68e3df5c28b02e5e62b7a
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-28 22:34:20 -0500

    Import books blueprint from routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,17 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
-
-    # Import models here
-    from app.models.book import Book
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development"
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
+    from app.models.book import Book
     from .routes import books_bp
     app.register_blueprint(books_bp)
 
