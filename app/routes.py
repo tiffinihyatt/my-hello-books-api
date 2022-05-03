ad516f20260dcc013ab18968cab17f280dab409b
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-03 09:47:22 -0500

    Created endpoint to update one book by id

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,7 @@ def create_book():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
+# get all books
 @books_bp.route("", methods=["GET"])
 def read_all_books():
     books_response = []
@@ -29,6 +30,7 @@ def read_all_books():
         )
     return jsonify(books_response)
 
+# get one book by id
 @books_bp.route("/<book_id>", methods=["GET"])
 def get_book_by_id(book_id):
     book = validate_book(book_id)
@@ -39,6 +41,22 @@ def get_book_by_id(book_id):
         "description": book.description
     }
 
+# update one book
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_single_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book_id} successfully updated")
+
+
+# helper function to check for valid book
 def validate_book(book_id):
     try:
         book_id = int(book_id)
