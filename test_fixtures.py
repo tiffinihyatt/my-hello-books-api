abe03b1288d9d9bf8062554601cebbfd8b92e7f6
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 11:42:39 -0500

    Added FancyObject class and so_fancy fixture

--- test_fixtures.py
+++ test_fixtures.py
@@ -1,5 +1,29 @@
 import pytest
 
+class FancyObject:
+    def __init__(self):
+        self.fancy = True
+        print(f"\nFancyObject: {self.fancy}")
+
+    def or_is_it(self):
+        self.fancy = not self.fancy
+
+    def cleanup(self):
+        print(f"\ncleanup: {self.fancy}")
+
+@pytest.fixture
+def so_fancy():
+    fancy_object = FancyObject()
+
+    yield fancy_object
+
+    fancy_object.cleanup()
+
+def test_so_fancy(so_fancy):
+    assert so_fancy.fancy
+    so_fancy.or_is_it()
+    assert not so_fancy.fancy
+
 @pytest.fixture
 def empty_list():
     return []
