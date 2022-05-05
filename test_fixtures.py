ab1a7bd0447979d1d55e8412f7ca5f557dab7db3
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 11:05:00 -0500

    Added one_item fixture and test

--- test_fixtures.py
+++ test_fixtures.py
@@ -7,3 +7,13 @@ def empty_list():
 def test_len_of_empty_list(empty_list):
     assert isinstance(empty_list, list)
     assert len(empty_list) == 0
+
+@pytest.fixture
+def one_item(empty_list):
+    empty_list.append("item")
+    return empty_list
+
+def test_len_of_unary_list(one_item):
+    assert isinstance(one_item, list)
+    assert len(one_item) == 1
+    assert one_item[0] == "item"
