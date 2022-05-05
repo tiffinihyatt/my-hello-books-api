8b9bb8f2c94bb57d78e4a65ba3e02132851f68ee
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 12:36:24 -0500

    Created test_get_one_book

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,14 @@ def test_get_all_books_with_empty_db(client):
     # assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_one_book(client, two_books):
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "title": "Americanah",
+        "description": "YA fiction"
+    }
