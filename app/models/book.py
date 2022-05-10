cb4e114382d0ea38d16780935584d4c9ddb38d60
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-10 09:45:52 -0500

    Create one to many relationship between Author and Book models

--- app/models/book.py
+++ app/models/book.py
@@ -1,7 +1,8 @@
 from app import db
 
-
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
+    author = db.relationship("Author", back_populates="book")
